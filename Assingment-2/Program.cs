f25d3b10ac6e88a8d9acdc981b2e76fb6d490eb2
Nick Lemke <koolnube84@gmail.com>
2018-02-20 07:21:37 -0800

    Upload

--- Assingment-2/Program.cs
+++ Assingment-2/Program.cs
@@ -8,15 +8,40 @@ namespace Assingment_2
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            string[] grades = { "Kindergarden", "First Grade", "Second Grade", "Third Grade", "Fourth Grade", "Fith Grade", "Sixth Grade", "Seven Grade", "Eigth Grade" };
+            string[] months = { "September", "October", "November", "December", "January", "Febuary", "March", "April", "May" };
+            int[] gradePrice = { 80, 60 };
+
+            int count = 0;
+            foreach (string grade in grades)
+            {
+                int tuition = 0;
+                if (count == 0)
+                {
+                    tuition = gradePrice[0];
+                }
+                else
+                {
+                    tuition = (gradePrice[1] * count); 
+                }
+                foreach (string month in months)
+                {
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        Console.WriteLine("---------------------------------------------");
+                        Console.WriteLine("Grade Number = " + count.ToString());
+                        Console.WriteLine("Classroom Number = " + i.ToString());
+                        Console.WriteLine("Month = " + month);
+                        Console.WriteLine("Tuition this moneth = $" + tuition.ToString());
+                        Console.WriteLine("---------------------------------------------");
+                        Console.WriteLine();
+                    }
+                }
+                count++;
+            }
         }
     }
 }
