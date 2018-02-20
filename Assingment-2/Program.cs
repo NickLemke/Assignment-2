38831680e0860a6d303e9cc7a21fdb6652de14f6
Nick Lemke <koolnube84@gmail.com>
2018-02-20 08:23:00 -0800

    Updated output order by month, and corrected formatting

--- Assingment-2/Program.cs
+++ Assingment-2/Program.cs
@@ -15,32 +15,30 @@ namespace Assingment_2
             string[] months = { "September", "October", "November", "December", "January", "Febuary", "March", "April", "May" };
             int[] gradePrice = { 80, 60 };
 
-            int count = 0;
-            foreach (string grade in grades)
+            int monthCount = 1;
+            foreach (string month in months)
             {
-                int tuition = 0;
-                if (count == 0)
+                Console.WriteLine("For Month " + month + "(" + monthCount.ToString() + "/" + months.Length.ToString() + ")");
+                int gradeCount = 0;
+                foreach (string grade in grades)
                 {
-                    tuition = gradePrice[0];
-                }
-                else
-                {
-                    tuition = (gradePrice[1] * count); 
-                }
-                foreach (string month in months)
-                {
+                    int tuition = 0;
+                    if (gradeCount == 0)
+                    {
+                        tuition = gradePrice[0];
+                    }
+                    else
+                    {
+                        tuition = (gradePrice[1] * gradeCount);
+                    }
                     for (int i = 1; i <= 3; i++)
                     {
-                        Console.WriteLine("---------------------------------------------");
-                        Console.WriteLine("Grade Number = " + count.ToString());
-                        Console.WriteLine("Classroom Number = " + i.ToString());
-                        Console.WriteLine("Month = " + month);
-                        Console.WriteLine("Tuition this moneth = $" + tuition.ToString());
-                        Console.WriteLine("---------------------------------------------");
-                        Console.WriteLine();
+                        Console.WriteLine("For Grade: " + gradeCount.ToString() + " Classroom: " + i.ToString() + " Tuition due: $" + tuition.ToString());
                     }
+                    gradeCount++;
                 }
-                count++;
+                monthCount++;
+                Console.WriteLine("*******************");
             }
         }
     }
